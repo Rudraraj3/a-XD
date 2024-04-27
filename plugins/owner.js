c94879b42b6f3c742c732526adc5da360ea7a37d
Rudraraj3 <147349281+Rudraraj3@users.noreply.github.com>
2024-04-27 17:59:00 +0530

    Update owner.js

--- plugins/owner.js
+++ plugins/owner.js
@@ -9,19 +9,19 @@ command(
   async (message, match, m, client) => {
   const zeta = {
   'contacts': {
-    'displayName': "𝐙𝐞𝐭𝐚-𝐗𝐃",
+    'displayName': "𝘼𝙉𝙅𝘼𝙉-𝐗𝐃",
     'contacts': [{
-      'vcard': "BEGIN:VCARD\nVERSION:3.0\nFN:𝐙𝐄𝐓𝐀-𝐗𝐃\nORG:Zeta-XD;\nTEL;type=CELL;type=VOICE;waid=919747257996:919747257996\nEND:VCARD"
+      'vcard': "BEGIN:VCARD\nVERSION:3.0\nFN:𝘼𝙉𝙅𝘼𝙉-𝐗𝐃\nORG:Zeta-XD;\nTEL;type=CELL;type=VOICE;waid=919747257996:919747257996\nEND:VCARD"
     }]
   },
   'contextInfo': {
     'externalAdReply': {
-      'title': "𝐙𝐞𝐭𝐚𝐚𝐡𝐡 ⛮",
-      'body': "ᴢᴇᴛᴀ-xᴅ ᴅᴇᴠᴇʟᴏᴩᴇʀ",
+      'title': "𝘼𝙉𝙅𝘼𝙉-𝐗𝐃",
+      'body': "𝐌𝐫.𝐚𝐧𝐣𝐚𝐧👀🦋",
       'thumbnailUrl': "https://i.imgur.com/xJV2r3g.jpeg",
       'mediaType': 0x1,
-      'mediaUrl': "http://wa.me/+919747257996?text=Hi+Badan-Ser+Im+From+Zeta-XD+Git",
-      'sourceUrl': "http://wa.me/+919747257996?text=Hi+Badan-Ser+Im+From+Zeta-XD+Git",
+      'mediaUrl': "http://wa.me/+919883457657?text=Hi+Anjan-Ser+Im+From+Anjan-XD+Git",
+      'sourceUrl': "http://wa.me/+919883457657?text=Hi+Anjan-Ser+Im+From+Anjan-XD+Git",
       'showAdAttribution': false
     }
   }
